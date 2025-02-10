6043c288b9682bd634f1dc6497bb9551c305035b
Daniel Ngira <81624602+danielngira@users.noreply.github.com>
2025-02-10 01:58:29 -0600

    fixed imports in game context

--- procrastination-simulator/src/game-context/GameContext.js
+++ procrastination-simulator/src/game-context/GameContext.js
@@ -3,7 +3,7 @@ import GameManager from "../classes/GameManager";
 import Player from "../classes/Player";
 import Day from "../classes/Day";
 import Task from "../classes/Task";
-import Time from "../classes/TIme";
+import Time from "../classes/Time";
 import Attributes from "../classes/Attributes";
 
 export const GameContext = createContext();
