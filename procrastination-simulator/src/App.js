b77ddcb21a4be6c7dbad70e82fffd7c2c34fd991
Spencer Dearman <dearmanspencer@gmail.com>
2025-02-11 16:29:18 -0600

    Updating

--- procrastination-simulator/src/App.js
+++ procrastination-simulator/src/App.js
@@ -3,9 +3,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">      
-        <p>
-          Game
-        </p>
+        <h1>Procrastination Simulator</h1>
+        <p>This is where we need to start including things for the game</p>
 
       </header>
     </div>
