0334ade4d0b191279cb39fab3d36113f65a58f01
Daniel Ngira <81624602+danielngira@users.noreply.github.com>
2025-02-13 22:29:09 -0600

    feat: made getter and setter functions

--- src/classes/Notification.js
+++ src/classes/Notification.js
@@ -17,4 +17,70 @@ export default class Notification extends Task {
     super(name);
     this.#notificationDuration = notificationDuration;
   }
+
+  /**
+   * Sets the follow-up activity.
+   * @param {string} followUp - The follow-up activity.
+   */
+  setFollowUp(followUp) {
+    if (typeof followUp === "string") {
+      this.#followUp = followUp;
+    } else {
+      console.error(
+        `Invalid follow-up. Expected a string but got ${typeof followUp}.`,
+      );
+    }
+  }
+
+  /**
+   * Gets the follow-up activity.
+   * @returns {string} The follow-up activity.
+   */
+  getFollowUp() {
+    return this.#followUp;
+  }
+
+  /**
+   * Sets whether the notification has been accepted.
+   * @param {boolean} accepted - Whether the notification is accepted.
+   */
+  setAccepted(accepted) {
+    if (typeof accepted === "boolean") {
+      this.#accepted = accepted;
+    } else {
+      console.error(
+        `Invalid value for accepted. Expected a boolean but got ${typeof accepted}.`,
+      );
+    }
+  }
+
+  /**
+   * Gets whether the notification has been accepted.
+   * @returns {boolean} True if the notification is accepted, otherwise false.
+   */
+  getAccepted() {
+    return this.#accepted;
+  }
+
+  /**
+   * Sets the notification time.
+   * @param {Date} notificationTime - The time of the notification.
+   */
+  setNotificationTime(notificationTime) {
+    if (notificationTime instanceof Date) {
+      this.#notificationTime = notificationTime;
+    } else {
+      console.error(
+        `Invalid notification time. Expected a Date object but got ${typeof notificationTime}.`,
+      );
+    }
+  }
+
+  /**
+   * Gets the notification time.
+   * @returns {Date} The time of the notification.
+   */
+  getNotificationTime() {
+    return this.#notificationTime;
+  }
 }
