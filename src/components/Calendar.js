d97ebd00ebc943287ba7e69d3ddb90f02a8d2a52
runanlin <107426955+runanlin@users.noreply.github.com>
2025-02-18 17:08:06 -0600

    feat: implemented calendar ui

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,9 +3,106 @@ import React from "react";
 function Calendar() {
   return (
     <div id="calendar">
-      {[...Array(13)].map((_, index) => (
-        <div key={index} className="time-block"></div>
-      ))}
+      <div class="time-block">
+        <div class="hours">6AM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">7AM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">8AM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">9AM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">10AM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">11AM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">12PM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">1PM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">2PM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">3PM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">4PM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">5PM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">6PM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">7PM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">8PM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">9PM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">10PM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">11PM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">12AM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">1AM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">2AM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">3AM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">4AM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">5AM</div>
+        <div class="slot"></div>
+      </div>
+      <div class="time-block">
+        <div class="hours">6AM</div>
+        <div class="slot"></div>
+      </div>
     </div>
   );
 }
