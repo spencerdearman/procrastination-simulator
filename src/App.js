0eb83415d91928205da04d5e1ad3bc7d4341ffb9
runanlin <107426955+runanlin@users.noreply.github.com>
2025-02-18 17:55:00 -0600

    feat: added Notification.css import

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./styles/global.css";
 import "./styles/Calendar.css";
 import "./styles/Header.css";
 import "./styles/PlayControls.css";
+import "./styles/Notification.css";
 import "./styles/Sidebar.css";
 import "./styles/Stats.css";
 import "./styles/TaskList.css";
