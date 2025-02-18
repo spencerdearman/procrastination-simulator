63f539fa6f1b59b53f0f832fc8bd0a8eb88eab00
markxiong0122 <markxiong0122@gmail.com>
2025-02-18 17:55:04 -0600

    fix: lint fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-import "./styles/global.css";
+import Calendar from "./components/Calendar";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 import "./styles/Calendar.css";
+import "./styles/global.css";
 import "./styles/Header.css";
 import "./styles/PlayControls.css";
 import "./styles/Sidebar.css";
 import "./styles/Stats.css";
 import "./styles/TaskList.css";
-import Calendar from "./components/Calendar";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
 
 function App() {
   const [currentDate, setCurrentDate] = useState("Tuesday");
