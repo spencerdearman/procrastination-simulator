1bc06ce433c850606c0b40176e53d2d76b8a59d9
Sam Xie <muyanxiesam@uchicago.edu>
2025-02-18 18:22:50 -0600

    feat: update time

--- src/classes/Time.js
+++ src/classes/Time.js
@@ -1,20 +1,52 @@
 export default class Time {
   constructor(scale = 60) {
     this.realSecondsPerGameHour = scale; /* Note, 60 real seconds = 1 game hr */
+
     this.startTime = Date.now(); // Store the real-world start time
-    this.gameStartTime = new Date(2025, 0, 1, 0, 0, 0); // Arbitrary in-game start time
+
+    this.lastGameRecordTime = new Date(2025, 0, 1, 0, 0, 0); // Arbitrary in-game start time
+
+    this.totalMinutesElapsed = 0;
+
+    this.lastRealWorldCheckTime = Date.now();
+
+    this.playerDefinedSpeed = 1;
   }
 
   /* Getting the current game time based on the elapsed real time and scale */
   getCurrentGameTime() {
-    let realElapsedSeconds = (Date.now() - this.startTime) / 1000;
-    let gameMinutesElapsed =
-      (realElapsedSeconds / this.realSecondsPerGameHour) * 60;
+    let realElapsedSeconds = (Date.now() - this.lastRealWorldCheckTime) / 1000;
 
-    let newGameTime = new Date(this.gameStartTime);
+    this.lastRealWorldCheckTime = Date.now();
 
-    newGameTime.setMinutes(newGameTime.getMinutes() + gameMinutesElapsed);
+    let newGameTime = new Date(
+      this.lastGameRecordTime.getTime() +
+        (realElapsedSeconds / this.realSecondsPerGameHour) *
+          (60 * this.playerDefinedSpeed) *
+          60 *
+          1000,
+    );
+
+    this.lastGameRecordTime = newGameTime;
 
     return newGameTime;
+  }
+
+  compareTwoTime() {}
+
+  isTriggered() {}
+
+  hasEnded() {}
+
+  xSpeed(speed) {
+    this.playerDefinedSpeed = speed;
+  }
+
+  stopTimer() {
+    this.playerDefinedSpeed = 0;
+  }
+
+  startTimer() {
+    this.playerDefinedSpeed = 1;
   }
 }
--- src/components/TimeView.js
+++ src/components/TimeView.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 const TimeView = ({ time }) => {
   const [currentGameTime, setCurrentGameTime] = useState(null);
+  const [isTimeStopped, setIsTimeStopped] = useState(false);
 
   const updateTime = () => {
     const newTime = time.getCurrentGameTime();
@@ -9,13 +10,36 @@ const TimeView = ({ time }) => {
   };
 
   useEffect(() => {
-    const intervalId = setInterval(updateTime, 1000);
-    return () => clearInterval(intervalId);
-  }, []);
+    setInterval(updateTime, 1000);
+  });
+
+  const toggleTime = () => {
+    if (isTimeStopped) {
+      time.startTimer();
+    } else {
+      time.stopTimer();
+    }
+    setIsTimeStopped(!isTimeStopped);
+  };
+
+  const twiceSpeed = () => {
+    time.xSpeed(2);
+  };
+
+  const halfSpeed = () => {
+    time.xSpeed(0.5);
+  };
 
   return (
     <div>
-      {currentGameTime ? currentGameTime.toLocaleString() : "Loading..."}
+      <div>
+        {currentGameTime ? currentGameTime.toLocaleString() : "Loading..."}
+      </div>
+      <button onClick={toggleTime}>
+        {isTimeStopped ? "Start Time" : "Stop Time"}
+      </button>
+      <button onClick={twiceSpeed}>Twice Speed</button>
+      <button onClick={halfSpeed}>Half Speed</button>
     </div>
   );
 };
