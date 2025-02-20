655db52a455e2cb83487259bb302da0f5d0036ce
chris collins <christopher.l.collins@gmail.com>
2025-02-20 17:52:11 -0600

    feat: drag and drop tasks
    you can now drag the tasks into their appropriate slots!

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,43 +1,49 @@
 import React from "react";
 
-// New TimeBlock component
-const TimeBlock = ({ hour }) => {
+//when the user drags the task, it will be added to the slot
+const handleDragOver = (e) => {
+  e.preventDefault();
+};
+
+//when the user drops the task, it will be added to the slot
+const handleDrop = (e) => {
+  e.preventDefault(); 
+  var data = e.dataTransfer.getData("text");
+  e.target.appendChild(document.getElementById(data));
+
+// do more stuff here...
+
+};
+
+//this is the time block that will be used to display the time and the slot
+const TimeBlock = ({ hour, droppable = true }) => {
   return (
     <div className="time-block">
       <div className="hours">{hour}</div>
-      <div className="slot"></div>
+      <div 
+        className={`slot ${!droppable ? 'disabled' : ''}`}
+        onDragOver={droppable ? handleDragOver : null}
+        onDrop={droppable ? handleDrop : null}
+      ></div>
     </div>
   );
 };
 
+//this is the calendar that will be used to display the time blocks and the slots
 function Calendar() {
+  //this is the time blocks that will be used to display the time and the slot
+  const timeBlocks = Array.from({ length: 24 }, (_, index) => {
+    const hour = index % 12 || 12; // Converts 0 to 12
+    const period = index < 12 ? 'AM' : 'PM';
+    return `${hour}${period}`;
+  });
+
   return (
     <div id="calendar-container">
       <div id="calendar">
-        <TimeBlock hour="12AM" />
-        <TimeBlock hour="1AM" />
-        <TimeBlock hour="2AM" />
-        <TimeBlock hour="3AM" />
-        <TimeBlock hour="4AM" />
-        <TimeBlock hour="5AM" />
-        <TimeBlock hour="6AM" />
-        <TimeBlock hour="7AM" />
-        <TimeBlock hour="8AM" />
-        <TimeBlock hour="9AM" />
-        <TimeBlock hour="10AM" />
-        <TimeBlock hour="11AM" />
-        <TimeBlock hour="12PM" />
-        <TimeBlock hour="1PM" />
-        <TimeBlock hour="2PM" />
-        <TimeBlock hour="3PM" />
-        <TimeBlock hour="4PM" />
-        <TimeBlock hour="5PM" />
-        <TimeBlock hour="6PM" />
-        <TimeBlock hour="7PM" />
-        <TimeBlock hour="8PM" />
-        <TimeBlock hour="9PM" />
-        <TimeBlock hour="10PM" />
-        <TimeBlock hour="11PM" />
+        {timeBlocks.map((hour) => (
+          <TimeBlock key={hour} hour={hour} />
+        ))}
       </div>
     </div>
   );
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,28 +1,30 @@
 import React from "react";
 
+//when the user drags the task, it will be added to the slot
+const handleDragStart = (ev) => {
+  ev.dataTransfer.setData("text", ev.target.id);
+};
+
+//this is the task list that will be used to display the tasks
 function TaskList() {
   return (
     <div id="tasks-container">
       <div id="tasks-list">
-        <TaskBlock
-          id="task1"
-          title="Write Essay"
-          description="info about this specific task"
-        />
-        <TaskBlock id="task2" title="Eat Food" description="yum" />
-        <TaskBlock id="task3" title="Sleep" description="zzz" />
-        <TaskBlock id="task4" title="Another Task" description="info" />
-        <TaskBlock id="task5" title="Another Task" description="info" />
-        <TaskBlock id="task6" title="Another Task" description="info" />
+        <TaskBlock id="task2" title="Clean Room" description="Your room is a literal pig sty that needs to be cleaned" type="mental" icon="🧹" />
+        <TaskBlock id="task3" title="Sleep" description="zzz" type="energy" icon="💤" />
+        <TaskBlock id="task4" title="Study Biology" description="Your midterm is next week and you know how that last one went..." type="academics" icon="📚" />
+        <TaskBlock id="task5" title="Theater Rehearsal" description="It's time to go to theater rehearsal, you've got a play coming up soon!" type="social" icon="👯‍♀️" />
+        <TaskBlock id="task6" title="Review an Art Crticism Article" description="Artists really love yapping..." type="academics" icon="🖼️" />
       </div>
     </div>
   );
 }
 
-function TaskBlock({ id, title, description }) {
+//this is the task block that will be used to display the task
+function TaskBlock({ id, title, description, type, icon }) {
   return (
-    <div className="task-block" id={id}>
-      <h2>{title}</h2>
+    <div className={`task-block type-${type}`} id={id} draggable="true" onDragStart={handleDragStart}>
+      <h2>{icon} {title}</h2>
       <p>{description}</p>
     </div>
   );
