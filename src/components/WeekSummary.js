e630c00e56d3f620fdeaa7f480ac07c3db6be1da
markxiong0122 <markxiong0122@gmail.com>
2025-02-25 17:19:57 -0600

    feat: llm params tuning

--- src/components/WeekSummary.js
+++ src/components/WeekSummary.js
@@ -1,5 +1,5 @@
 import Groq from "groq-sdk";
-import { useEffect, useState } from "react";
+import {useEffect,useState} from "react";
 import Stats from "./Stats";
 
 function WeekSummary() {
@@ -25,10 +25,11 @@ function WeekSummary() {
           messages: [
             {
               role: "user",
-              content: `Write one-paragraph summary of ${name}'s day, based on their status and schedule. Their status is made up of 4 values (each out of 100), Academics, Social Life, Energy, and Mental Health. Here are the status values: Academics: ${stats.academics}, Social Life: ${stats.social}, Energy: ${stats.energy}, Mental Health: ${stats.mentalHealth}. Adjust the tone and description of their day based on how high each of these values are, assuming 100=best and 0=worst. Their schedule was: ${log}.`,
+              content: `Write 50-word summary of ${name}'s day, based on their status and schedule. Their status is made up of 4 values (each out of 100), Academics, Social Life, Energy, and Mental Health. Here are the status values: Academics: ${stats.academics}, Social Life: ${stats.social}, Energy: ${stats.energy}, Mental Health: ${stats.mentalHealth}. Adjust the tone and description of their day based on how high each of these values are, assuming 100=best and 0=worst. Write it in first-person and do not specifically reference the stats. Their schedule was: ${log}.`,
             },
           ],
           model: "llama-3.1-8b-instant", // Smallest Model Available
+          temperature: 1.5,
         });
 
         setSummary(
@@ -46,10 +47,6 @@ function WeekSummary() {
 
   return (
     <div>
-      <div id="header">
-        <p id="intro-text">You are a...</p>
-        <h3 id="banner-text">Lebron</h3>
-      </div>
       <div id="content">
         <div id="stats-box">
           <h3 id="blurb-text">Your Cumulative Stats</h3>
@@ -60,6 +57,10 @@ function WeekSummary() {
           <p id="blurb-text">{summary || "Writing Your Journal Entry 📝..."}</p>
           <button className="restart-button">Restart Game</button>
         </div>
+      </div>
+      <div id="header">
+        <p id="intro-text">You are a...</p>
+        <h3 id="banner-text">Lebron</h3>
       </div>
     </div>
   );
