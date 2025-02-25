ef223183772f6bb858f22170b7f0b2dac03e9071
amber auh <amberauh@uchicago.edu>
2025-02-25 17:07:56 -0600

    feat: created startpage

---
+++ src/components/StartPage.js
@@ -0,0 +1,22 @@
+import React from "react";
+// import Stats from "./Stats";
+
+
+function StartPage() {
+  return (
+    <body>
+      <div id="header">
+        <p id="intro-text">Welcome to</p>
+        <h3 id="banner-text">Welcome to Procrastination Simulator</h3>
+      </div>
+      <div id="content">
+        <div id="summary-text-box">
+          <h3 id="blurb-text">Your Week</h3>
+          <p id="blurb-text"> start game description </p>
+        </div>
+      </div>
+    </body>
+  );
+}
+
+export default StartPage;
