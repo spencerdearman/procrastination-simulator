85f962a4d655bbda6943ad4d9eab1da477209d0b
chris collins <christopher.l.collins@gmail.com>
2025-02-25 00:05:00 -0600

    feat: stat bar component
    cleaned up stat bar component, added progress bar animation

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -5,26 +5,24 @@ function Stats() {
     <div id="stats">
       <p className="side-bar-header">Your Stats</p>
       <div id="stats-block">
-        <StatBar title="Academics" colorClass="academics-color" />
-        <StatBar title="Social Life" colorClass="social-life-color" />
-        <StatBar title="Energy" colorClass="energy-color" />
-        <StatBar title="Mental Health" colorClass="mental-health-color" />
+        <StatBar title="Academics" colorClass="academics-color" score="22" />
+        <StatBar title="Social Life" colorClass="social-life-color" score="50" />
+        <StatBar title="Energy" colorClass="energy-color" score="75" />
+        <StatBar title="Mental Health" colorClass="mental-health-color" score="100" />
       </div>
     </div>
   );
 }
 
-function StatBar({ title, colorClass }) {
+
+
+function StatBar({ title, colorClass, score }) {
   return (
     <div className={`div-${title.toLowerCase().replace(" ", "-")}`}>
       <p className="side-bar-subheading">{title}</p>
-      <div
-        className={`stats-bar sb-${title.toLowerCase().replace(" ", "-")}`}
-      ></div>
-      <div
-        className={`stats-bar-fill ${colorClass} sb-${title.toLowerCase().replace(" ", "-")}`}
-      ></div>
-      <div className="stats-score">9/100</div>
+      <div className="progress-container">
+      <progress className={`stats-bar-fill ${colorClass} sb-${title.toLowerCase().replace(" ", "-")}`} value={score} max="100"></progress><label>{score}/100</label>
+      </div>
     </div>
   );
 }
