83e5da7953be6331c24d5c3ed7b39bb9807d8fcf
cuyakonwu <cuyakonwu@uchicago.edu>
2025-02-25 17:55:30 -0600

    feat: creating daySummary

--- src/game-context/GameContext.js
+++ src/game-context/GameContext.js
@@ -1,24 +1,5 @@
-import React, { createContext, useState } from "react";
-import Logic from "../classes/Logic";
-import Player from "../classes/Player";
-import Day from "../classes/Day";
-import Time from "../classes/Time";
+import { createContext } from "react";
 
-// Create a context for the game.
 export const GameContext = createContext();
 
-// The GameProvider sets up the shared game logic and exposes it to its children.
-export const GameProvider = ({ children }) => {
-  // Initialize the game logic only once.
-  const [logic] = useState(() => {
-    const time = new Time(60); // Shared Time instance
-    const player = new Player("Player 1");
-    const day = new Day();
-    // Create the game logic with the shared instances.
-    return new Logic(player, [day], time);
-  });
-
-  return (
-    <GameContext.Provider value={{ logic }}>{children}</GameContext.Provider>
-  );
-};
+//Need to make a GameProvider
