022d41c926e23d33dd6aeecbe455a5240717de29
chris collins <christopher.l.collins@gmail.com>
2025-02-25 23:48:54 -0600

    Update Tutorial.js
    ok fine react, you win.

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -1,18 +1,27 @@
-import React from "react";
-import './scripts/tutorialScripting.js';
+import React, { useState } from "react";
 // import 'scripts/tutorialScripting.js';
 
 function Tutorial() {
+  const totalSlides = 4;
+  const [currentSlide, setCurrentSlide] = useState(1);
+  const handlePreviousSlide = () => {
+    setCurrentSlide(prev => Math.max(prev - 1, 1));
+  };
+
+  const handleNextSlide = () => {
+    setCurrentSlide(prev => Math.min(totalSlides, prev +1));
+  };
+
   return (
     <div className="tutorial">
-      <button className="tutorial-button" id="tutorial-left">
+      <button className="tutorial-button" id="tutorial-left" onClick={handlePreviousSlide}>
         &lt;
       </button>
-      <img src="./images/slide1.png" alt="" id="tutorial-image" />
-      <button className="tutorial-button" id="tutorial-right">
+      <img src={`./images/slide${currentSlide}.png`} alt="" id="tutorial-image" />
+      <button className="tutorial-button" id="tutorial-right" onClick={handleNextSlide}>
         &gt;
       </button>
-     
+
     </div>
   );
 }
