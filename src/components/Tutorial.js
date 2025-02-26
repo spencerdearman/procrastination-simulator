ac3cfaf4147558fe56680e58e4b9acfa2b6d5ad6
chris collins <christopher.l.collins@gmail.com>
2025-02-25 22:55:02 -0600

    fix: loading tutorialScript.js
    fixed tutorialScript

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -1,16 +1,18 @@
 import React from "react";
+import './scripts/tutorialScripting.js';
+// import 'scripts/tutorialScripting.js';
 
 function Tutorial() {
   return (
     <div className="tutorial">
-      <button class="tutorial-button" id="tutorial-left">
+      <button className="tutorial-button" id="tutorial-left">
         &lt;
       </button>
       <img src="./images/slide1.png" alt="" id="tutorial-image" />
       <button className="tutorial-button" id="tutorial-right">
         &gt;
       </button>
-      <script src="./scripts/tutorial.js" defer />
+     
     </div>
   );
 }
