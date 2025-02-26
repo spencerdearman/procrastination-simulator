854f1e438fcab75c519bfa29f45fd9430cfd23ab
chrisbeltr <christianbeltran@uchicago.edu>
2025-02-26 00:38:25 -0600

    feat: finished tutorial page

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -2,26 +2,65 @@ import React, { useState } from "react";
 // import 'scripts/tutorialScripting.js';
 
 function Tutorial() {
+  const [tutorialVisibility, setTutorialVisibility] = useState("visible");
+  const [leftDisabled, setLeftDisabled] = useState(true);
+  const [rightDisabled, setRightDisabled] = useState(false);
   const totalSlides = 4;
   const [currentSlide, setCurrentSlide] = useState(1);
   const handlePreviousSlide = () => {
-    setCurrentSlide(prev => Math.max(prev - 1, 1));
+    setCurrentSlide((prev) => Math.max(prev - 1, 1));
+    if (currentSlide <= 2) {
+      setLeftDisabled(true);
+    } else {
+      setLeftDisabled(false);
+      setRightDisabled(false);
+    }
   };
 
   const handleNextSlide = () => {
-    setCurrentSlide(prev => Math.min(totalSlides, prev +1));
+    setCurrentSlide((prev) => Math.min(totalSlides, prev + 1));
+    if (currentSlide >= totalSlides - 1) {
+      setRightDisabled(true);
+    } else {
+      setLeftDisabled(false);
+      setRightDisabled(false);
+    }
+  };
+
+  const handleVisibility = () => {
+    setTutorialVisibility("hidden");
   };
 
   return (
-    <div className="tutorial">
-      <button className="tutorial-button" id="tutorial-left" onClick={handlePreviousSlide}>
-        &lt;
-      </button>
-      <img src={`./images/slide${currentSlide}.png`} alt="" id="tutorial-image" />
-      <button className="tutorial-button" id="tutorial-right" onClick={handleNextSlide}>
-        &gt;
-      </button>
-
+    <div className="tutorial" style={{ visibility: `${tutorialVisibility}` }}>
+      <div className="tutorial-top">
+        <button
+          disabled={leftDisabled}
+          className="tutorial-button"
+          id="tutorial-left"
+          onClick={handlePreviousSlide}
+        >
+          &lt;
+        </button>
+        <img
+          src={`./images/slide${currentSlide}.png`}
+          alt=""
+          id="tutorial-image"
+        />
+        <button
+          disabled={rightDisabled}
+          className="tutorial-button"
+          id="tutorial-right"
+          onClick={handleNextSlide}
+        >
+          &gt;
+        </button>
+      </div>
+      <div className="tutorial-bottom">
+        <button id="tutorial-end" onClick={handleVisibility}>
+          End Tutorial
+        </button>
+      </div>
     </div>
   );
 }
