b2c5b78bde51a573e677a828dcf398b7e4dff9b1
Ben Burgess <benburgess519@gmail.com>
2025-02-25 18:22:39 -0600

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import Gameplay from "pages/gameplay/Gameplay";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Gameplay />;
+  return (
+    <Routes>
+      <Route path="" element={<h1>Introduction</h1>} />
+      <Route path="/game/tutorial" element={<h1>Tutorial</h1>} />
+      <Route path="/game/calendar" element={<Gameplay />} />
+      <Route path="/game/end-of-day" element={<h1>End of day</h1>} />
+      <Route path="/game/end-of-week" element={<h1>End of week</h1>} />
+      <Route
+        path="*"
+        element={<h1>Make sure this redirects to introduction</h1>}
+      />
+    </Routes>
+  );
 }
 
 export default App;
