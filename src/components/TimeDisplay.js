eea8f1a2390a844ab230758a7e51879939c4fb1c
Ben Burgess <benburgess519@gmail.com>
2025-03-03 15:44:36 -0600

    fix: remove debugging `TimeView`

--- src/components/TimeDisplay.js
+++ src/components/TimeDisplay.js
@@ -1,16 +1,11 @@
-import { useGame } from "game-context/GameContext";
 import React from "react";
-import TimeView from "./TimeView";
 
 export default function TimeDisplay({ day }) {
-  const { currentTime } = useGame();
-
   return (
     <div id="time">
       <button className="time-button">&lt;</button>
       <div className="date-wrapper">
         <span className="current-date">{day.getDayOfWeek()}</span>
-        <TimeView time={currentTime} />
       </div>
       <button className="time-button">&gt;</button>
     </div>
