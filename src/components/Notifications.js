aed53fd8e4d3cdb9013831339768fa15271a5e4c
Daniel Ngira <81624602+danielngira@users.noreply.github.com>
2025-03-03 00:47:53 -0600

    impr: improved notification component

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -21,62 +21,37 @@ function NotificationsList({ notifications, onAccept, onReject }) {
 
 function Notification({
   isMandatory = false,
-  title = "Matthew's birthday party",
-  message = "Lebron lebron lebron",
-  icon = "🥳",
-  dismissButton = "Dismiss",
+  title,
+  message,
+  icon = "📢",
   acceptButton = "Accept",
   rejectButton = "Reject",
   onAccept,
   onReject,
 }) {
-  const randomPosition = {
-    top: `${Math.floor(Math.random() * 80)}vh`, // Random value between 0-80% of viewport height
-    left: `${Math.floor(Math.random() * 80)}vw`, // Random value between 0-80% of viewport width
-  };
-
-  if (isMandatory) {
-    // Mandatory Notification
-    return (
-      <div className="alert-popup" style={randomPosition}>
-        <div className="notification-main">
-          <span className="emoji-icon" role="img">
-            {icon}
-          </span>
-          <div className="notification-text">
-            <h3 className="notification-title">{title}</h3>
-            <p className="notification-message">{message}</p>
-          </div>
-        </div>
-        <div className="notification-options">
-          <button className="accept-button">Dismiss</button>
+  return (
+    <div className={isMandatory ? "alert-popup" : "notification-popup"}>
+      <div className="notification-main">
+        <span className="emoji-icon" role="img">
+          {icon}
+        </span>
+        <div className="notification-text">
+          <h3 className="notification-title">{title}</h3>
+          <p className="notification-message">{message}</p>
         </div>
       </div>
-    );
-  } else {
-    // Optional Notification
-    return (
-      <div className="notification-popup" style={randomPosition}>
-        <div className="notification-main">
-          <span className="emoji-icon" role="img">
-            {icon}
-          </span>
-          <div className="notification-text">
-            <h3 className="notification-title">{title}</h3>
-            <p className="notification-message">{message}</p>
-          </div>
-        </div>
-        <div className="notification-options">
-          <button className="reject-button" id="reject-button">
+      <div className="notification-options">
+        {!isMandatory && (
+          <button className="reject-button" onClick={onReject}>
             {rejectButton}
           </button>
-          <button className="accept-button" id="accept-button">
-            {acceptButton}
-          </button>
-        </div>
+        )}
+        <button className="accept-button" onClick={onAccept}>
+          {acceptButton}
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default NotificationsList;
