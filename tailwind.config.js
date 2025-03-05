fc9f39397748e015c3593c3c8e9839aa2d910123
Ben Burgess <benburgess519@gmail.com>
2025-03-05 16:07:32 -0600

    refactor: stat colors into tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx,css}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          DEFAULT: "#9fbfd6", // Background for Academics
+          dark: "#314d65", // Border for Academics
+        },
+        red: {
+          DEFAULT: "#e7c5ce", // Background for Social Life
+          dark: "#a1544a", // Border for Social Life
+        },
+        yellow: {
+          DEFAULT: "#81beff", // Background for Energy
+          dark: "#b98d35", // Border for Energy
+        },
+        green: {
+          DEFAULT: "#d3e5d7", // Background for Mental Health
+          dark: "#4d676c", // Border for Mental Health
+        },
+      },
+    },
   },
   plugins: [],
 };
