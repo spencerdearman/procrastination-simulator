9e1c92f60ee4533a5ace471e8d1c131ba52fbcbf
Ben Burgess <benburgess519@gmail.com>
2025-03-09 18:58:39 -0500

    refactor: `Calendar` JSX

--- src/pages/gameplay/components/Calendar.js
+++ src/pages/gameplay/components/Calendar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useGame } from "game-context/GameContext";
 import "styles/Calendar.css";
 import TaskBlock from "./TaskBlock";
@@ -8,12 +8,16 @@ import TimeIndicator from "./TimeIndicator";
 
 export default function Calendar({ draggedTaskGhostRef }) {
   const gameContext = useGame();
-  const plannedTasks = gameContext.getPlannedTasks();
+  const [plannedTasks, setPlannedTasks] = useState();
   const formatHour = (hour) => {
     const period = hour >= 12 ? "PM" : "AM";
     const displayHour = hour === 0 ? 12 : hour > 12 ? hour - 12 : hour;
     return `${displayHour}${period}`;
   };
+
+  useEffect(() => {
+    setPlannedTasks(gameContext.tasks.filter((task) => task.startTime));
+  }, [gameContext.tasks]);
 
   const handleDragOver = (e, i) => {
     e.preventDefault();
@@ -23,6 +27,33 @@ export default function Calendar({ draggedTaskGhostRef }) {
       draggedTaskGhostRef.current.classList.remove("invalid");
     } else {
       draggedTaskGhostRef.current.classList.add("invalid");
+    }
+  };
+
+  const handleDrop = (e, i) => {
+    e.preventDefault();
+    try {
+      const taskData = JSON.parse(e.dataTransfer.getData("application/json"));
+      gameContext.logicPlanTask(taskData, i);
+
+      //Adding audio
+      const taskPlacedSound = new Audio("/sound/new_task.mp3");
+      taskPlacedSound.volume = 0.5; // Adjust volume as needed
+      taskPlacedSound.play();
+    } catch (error) {
+      console.error("Error handling drop:", error);
+    } finally {
+      // Cleanup on unmount
+      if (draggedTaskGhostRef.current) {
+        if (draggedTaskGhostRef.current.dragHandler) {
+          document.removeEventListener(
+            "drag",
+            draggedTaskGhostRef.current.dragHandler,
+          );
+        }
+        draggedTaskGhostRef.current.remove();
+        draggedTaskGhostRef.current = null;
+      }
     }
   };
 
@@ -38,34 +69,24 @@ export default function Calendar({ draggedTaskGhostRef }) {
             <div
               className="slot"
               onDragOver={(e) => handleDragOver(e, i)}
-              onDrop={(e) => {
-                e.preventDefault();
-                try {
-                  const taskData = JSON.parse(
-                    e.dataTransfer.getData("application/json"),
-                  );
-                  gameContext.logicPlanTask(taskData, i);
-                  console.log(`Dropped task "${taskData.name}" at hour ${i}`);
-
-                  //Adding audio
-                  const taskPlacedSound = new Audio("/sound/new_task.mp3");
-                  taskPlacedSound.volume = 0.5; // Adjust volume as needed
-                  taskPlacedSound.play();
-                } catch (error) {
-                  console.error("Error handling drop:", error);
-                }
-              }}
+              onDrop={(e) => handleDrop(e, i)}
             >
-              {plannedTasks
-                .filter((task) => new Date(task.startTime).getHours() === i)
-                .map((task, index) => (
-                  <TaskBlock
-                    key={`${task.name}-${index}`}
-                    task={task}
-                    draggable={!task.locked}
-                    draggedTaskGhostRef={draggedTaskGhostRef}
-                  />
-                ))}
+              {plannedTasks &&
+                (() => {
+                  const task = plannedTasks.find(
+                    (task) => new Date(task.startTime).getHours() === i,
+                  );
+                  return (
+                    task && (
+                      <TaskBlock
+                        key={task.name}
+                        task={task}
+                        draggable={!task.locked}
+                        draggedTaskGhostRef={draggedTaskGhostRef}
+                      />
+                    )
+                  );
+                })()}
             </div>
           </div>
         ))}
